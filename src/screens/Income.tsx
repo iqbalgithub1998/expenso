9fce71367c3ab84ee1e9253e68e9cdc539f8bfaf
Mohammmed Iqbal khan <iqbal.k@bayshoreintel.com>
2023-06-27 15:17:34 +0530

    transfer and expense and income screen fix

--- src/screens/Income.tsx
+++ src/screens/Income.tsx
@@ -1,15 +1,273 @@
+// import {
+//   StyleSheet,
+//   Text,
+//   View,
+//   TouchableOpacity,
+//   TextInput,
+//   Alert,
+//   StatusBar,
+// } from 'react-native';
+// import React, {useState} from 'react';
+// import {COLORS, SIZES} from '../constants/theme';
+// import Ionicons from 'react-native-vector-icons/Ionicons';
+// import CustomButton from '../components/CustomButton';
+// import CustomNumberInput from '../components/CustomNumberInput';
+// import CustomDropDown from '../components/CustomDropDown';
+// import {Categories} from '../constants/Categories';
+// import {savingsTypes} from '../constants/Categories';
+// import CustomTextInput from '../components/CustomTextInput';
+
+// import RepeatTransaction from '../components/RepeatTransaction';
+// import DateSelect from '../components/Date';
+// import {uploadCustomData} from '../Api/FireBaseInsertion';
+// import auth from '@react-native-firebase/auth';
+// import {GoogleSignin} from '@react-native-google-signin/google-signin';
+
+// const Income: React.FC<any> = ({navigation}) => {
+//   const [expenseValue, setExpenseValue] = useState<number | undefined>(
+//     undefined,
+//   );
+//   const [categoryValue, setCategoryValue] = useState('');
+//   const [transactionTypeValue, setTransactionTypeValue] = useState('');
+//   const [descriptionValue, setDescriptionValue] = useState('');
+//   const [dateValue, setDateValue] = useState('');
+
+//   const handlePress = () => {
+//     navigation.goBack();
+//   };
+
+//   const handleSubmit = async () => {
+//     if (
+//       expenseValue &&
+//       categoryValue &&
+//       transactionTypeValue &&
+//       // descriptionValue &&  -----Add this when We need description to be there
+//       dateValue
+//     ) {
+//       console.log('Expense:', expenseValue);
+//       console.log('Category:', categoryValue);
+//       console.log('Transaction Type:', transactionTypeValue);
+//       console.log('Description:', descriptionValue);
+//       console.log('Date:', dateValue);
+
+//       // const incomeData = {
+//       //   expense: expenseValue,
+//       //   category: categoryValue,
+//       //   transactionType: transactionTypeValue,
+//       //   description: descriptionValue,
+//       //   deadline: dateValue,
+//       //   createdAt: firestore.FieldValue.serverTimestamp(),
+//       // };
+//       const typeValue = 'Lent';
+
+//       const currentUser = auth().currentUser;
+//       let userId = '';
+
+//       if (currentUser) {
+//         userId = currentUser.uid || '';
+//       } else {
+//         try {
+//           await GoogleSignin.hasPlayServices();
+//           const currentUser = await GoogleSignin.getCurrentUser();
+//           //console.log(currentUser);
+//           userId = currentUser?.user.id || '';
+//         } catch (error) {
+//           console.log('Google Sign-In error:', error);
+//           return 'Google Sign-In error';
+//         }
+//       }
+
+//       await uploadCustomData(
+//         userId,
+//         expenseValue,
+//         categoryValue,
+//         transactionTypeValue,
+//         descriptionValue,
+//         dateValue,
+//         typeValue,
+//       );
+
+//       Alert.alert('Data uploaded succesfuly');
+//       navigation.navigate('InitialHome');
+//     } else {
+//       Alert.alert('Fill the fields');
+//     }
+//   };
+
+//   // const handleAttachment = () => {
+//   //   console.log("Will add attachment");
+//   // }
+
+//   return (
+//     <View style={styles.container}>
+//       <StatusBar backgroundColor={COLORS.green} barStyle="light-content" />
+//       <View style={styles.topSection}>
+//         <View
+//           style={{
+//             flexDirection: 'row',
+//             alignItems: 'center',
+//             justifyContent: 'space-evenly',
+//             marginBottom: 15,
+//           }}>
+//           <TouchableOpacity
+//             onPress={handlePress}
+//             style={{
+//               position: 'absolute',
+//               left: -2,
+//             }}>
+//             <Ionicons
+//               name="ios-arrow-back-outline"
+//               size={45}
+//               color={COLORS.white}
+//             />
+//           </TouchableOpacity>
+//           <Text style={styles.expense}>Income</Text>
+//         </View>
+//         <View style={{marginTop: '5%', paddingLeft: 10}}>
+//           <Text style={{color: COLORS.white, fontWeight: '600', fontSize: 16}}>
+//             How Much ?
+//           </Text>
+//           <View style={styles.textInput}>
+//             <Text
+//               style={{fontSize: 75, color: COLORS.white, fontWeight: 'bold'}}>
+//               ₹{' '}
+//             </Text>
+//             <CustomNumberInput
+//               Style={styles.input}
+//               placeholder="0"
+//               placeholderTextColor="white"
+//               onChangeText={value => {
+//                 const parsedValue = parseFloat(value);
+//                 setExpenseValue(isNaN(parsedValue) ? undefined : parsedValue);
+//               }}
+//             />
+//           </View>
+//         </View>
+//       </View>
+//       <View style={styles.bottomSection}>
+//         <View style={{flex: 1, marginHorizontal: 20, marginVertical: 20}}>
+//           <View style={{flex: 1, justifyContent: 'space-evenly'}}>
+//             <CustomDropDown
+//               options={Categories}
+//               placeholder="Category"
+//               onSelectValue={value => setCategoryValue(value)}
+//             />
+//             <CustomDropDown
+//               options={savingsTypes}
+//               placeholder="Transaction Type"
+//               onSelectValue={value => setTransactionTypeValue(value)}
+//             />
+//             <CustomTextInput
+//               placeholder="Description"
+//               placeholderTextColor="grey"
+//               onChangeText={value => setDescriptionValue(value)}
+//             />
+//             {/* <AddAttachment
+//                       title="Add Attachment"
+//                       onPress={handleAttachment}
+//                     />  */}
+//             <DateSelect
+//               placeholder="Select date"
+//               onSelectDate={value => setDateValue(value)}
+//             />
+//             <RepeatTransaction title="Repeat" subTitle="Repeat Transaction" />
+//           </View>
+
+//           <CustomButton
+//             title="Continue"
+//             onPress={handleSubmit}
+//             // Style={styles.button}
+//             // titleStyle={styles.ButtonText}
+//             Style={[styles.button]}
+//             titleStyle={[styles.ButtonText]}
+//             backgroundColor={COLORS.primary}
+//           />
+//         </View>
+//       </View>
+//     </View>
+//   );
+// };
+
+// export default Income;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     flexDirection: 'column',
+//     backgroundColor: COLORS.green,
+//     flexBasis: '100%',
+//   },
+//   topSection: {
+//     flex: 3,
+//     marginVertical: 22,
+//     marginHorizontal: 20,
+//     // Replace with your desired styles
+//   },
+//   bottomSection: {
+//     flex: 7,
+//     backgroundColor: 'white',
+//     borderTopLeftRadius: 40,
+//     borderTopRightRadius: 40,
+//     //justifyContent:'space-between'
+//   },
+//   expense: {
+//     fontSize: 25,
+//     fontWeight: 'bold',
+//     marginVertical: 12,
+//     color: COLORS.white,
+//   },
+//   input: {
+//     backgroundColor: 'transparent',
+//     fontSize: 75,
+//     fontWeight: 'bold',
+//     color: 'white',
+//   },
+//   textInput: {
+//     //marginTop: 1,
+//     width: SIZES.width / 1.4,
+//     flexDirection: 'row',
+//     alignItems: 'center',
+//     justifyContent: 'flex-start',
+//   },
+//   button: {
+//     width: '100%',
+//     // minHeight: 60,
+//     // backgroundColor: COLORS.primary,
+//     // justifyContent: 'center',
+//     // alignItems: 'center',
+//     // marginBottom: 5,
+//     // borderRadius: 15,
+//     elevation: 8,
+//     // marginTop:6,
+//     // zIndex:0
+//     // width: SIZES.width - 30,
+//     minHeight: 60,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     marginBottom: 5,
+//     borderRadius: 15,
+//   },
+//   ButtonText: {
+//     fontWeight: '500',
+//     fontSize: 22,
+//     color: COLORS.white,
+//   },
+// });
+
 import {
   StyleSheet,
   Text,
   View,
   TouchableOpacity,
-  TextInput,
   Alert,
+  KeyboardAvoidingView,
   StatusBar,
+  Platform,
 } from 'react-native';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {COLORS, SIZES} from '../constants/theme';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import type {NativeStackScreenProps} from '@react-navigation/native-stack';
 import CustomButton from '../components/CustomButton';
 import CustomNumberInput from '../components/CustomNumberInput';
 import CustomDropDown from '../components/CustomDropDown';
@@ -17,13 +275,15 @@ import {Categories} from '../constants/Categories';
 import {savingsTypes} from '../constants/Categories';
 import CustomTextInput from '../components/CustomTextInput';
 
-import RepeatTransaction from '../components/RepeatTransaction';
 import DateSelect from '../components/Date';
 import {uploadCustomData} from '../Api/FireBaseInsertion';
 import auth from '@react-native-firebase/auth';
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import SuccessModal from '../components/SuccessModal';
+import ExpenseDropDown from '../components/ExpenseDropDown';
 
 const Income: React.FC<any> = ({navigation}) => {
+  // const height = useHeaderHeight()
   const [expenseValue, setExpenseValue] = useState<number | undefined>(
     undefined,
   );
@@ -31,6 +291,7 @@ const Income: React.FC<any> = ({navigation}) => {
   const [transactionTypeValue, setTransactionTypeValue] = useState('');
   const [descriptionValue, setDescriptionValue] = useState('');
   const [dateValue, setDateValue] = useState('');
+  const [showModal, setShowModal] = useState(false);
 
   const handlePress = () => {
     navigation.goBack();
@@ -44,20 +305,12 @@ const Income: React.FC<any> = ({navigation}) => {
       // descriptionValue &&  -----Add this when We need description to be there
       dateValue
     ) {
-      console.log('Expense:', expenseValue);
-      console.log('Category:', categoryValue);
-      console.log('Transaction Type:', transactionTypeValue);
-      console.log('Description:', descriptionValue);
-      console.log('Date:', dateValue);
+      // console.log('Expense:', expenseValue);
+      // console.log('Category:', categoryValue);
+      // console.log('Transaction Type:', transactionTypeValue);
+      // console.log('Description:', descriptionValue);
+      // console.log('Date:', dateValue);
 
-      // const incomeData = {
-      //   expense: expenseValue,
-      //   category: categoryValue,
-      //   transactionType: transactionTypeValue,
-      //   description: descriptionValue,
-      //   deadline: dateValue,
-      //   createdAt: firestore.FieldValue.serverTimestamp(),
-      // };
       const typeValue = 'Lent';
 
       const currentUser = auth().currentUser;
@@ -71,12 +324,12 @@ const Income: React.FC<any> = ({navigation}) => {
           const currentUser = await GoogleSignin.getCurrentUser();
           //console.log(currentUser);
           userId = currentUser?.user.id || '';
+          console.log('woring 0');
         } catch (error) {
           console.log('Google Sign-In error:', error);
-          return 'Google Sign-In error';
+          return 'google sign error';
         }
       }
-
       await uploadCustomData(
         userId,
         expenseValue,
@@ -86,21 +339,22 @@ const Income: React.FC<any> = ({navigation}) => {
         dateValue,
         typeValue,
       );
-
+      console.log('woring 2');
+      console.log('Expense data uploaded successfully');
       Alert.alert('Data uploaded succesfuly');
+      //setShowModal(true);
       navigation.navigate('InitialHome');
     } else {
       Alert.alert('Fill the fields');
     }
   };
 
-  // const handleAttachment = () => {
-  //   console.log("Will add attachment");
-  // }
+  // const handleCloseModal = () => {
+  //   setShowModal(false);
+  // };
 
   return (
     <View style={styles.container}>
-      <StatusBar backgroundColor={COLORS.green} barStyle="light-content" />
       <View style={styles.topSection}>
         <View
           style={{
@@ -123,13 +377,13 @@ const Income: React.FC<any> = ({navigation}) => {
           </TouchableOpacity>
           <Text style={styles.expense}>Income</Text>
         </View>
-        <View style={{marginTop: '5%', paddingLeft: 10}}>
+        <View style={{marginTop: '5%', paddingLeft: 5}}>
           <Text style={{color: COLORS.white, fontWeight: '600', fontSize: 16}}>
             How Much ?
           </Text>
           <View style={styles.textInput}>
             <Text
-              style={{fontSize: 75, color: COLORS.white, fontWeight: 'bold'}}>
+              style={{fontSize: 65, color: COLORS.white, fontWeight: 'bold'}}>
               ₹{' '}
             </Text>
             <CustomNumberInput
@@ -138,50 +392,50 @@ const Income: React.FC<any> = ({navigation}) => {
               placeholderTextColor="white"
               onChangeText={value => {
                 const parsedValue = parseFloat(value);
-                setExpenseValue(isNaN(parsedValue) ? undefined : parsedValue);
+                setExpenseValue(isNaN(parsedValue) ? 0 : parsedValue);
               }}
             />
           </View>
         </View>
       </View>
       <View style={styles.bottomSection}>
-        <View style={{flex: 1, marginHorizontal: 20, marginVertical: 20}}>
-          <View style={{flex: 1, justifyContent: 'space-evenly'}}>
-            <CustomDropDown
+        <View style={{flex: 1, marginHorizontal: 10, marginTop: 20}}>
+          <KeyboardAvoidingView
+            keyboardVerticalOffset={Platform.select({ios: 0, android: 600})}
+            behavior="padding"
+            style={{flex: 1}}
+            enabled>
+            <ExpenseDropDown
               options={Categories}
               placeholder="Category"
               onSelectValue={value => setCategoryValue(value)}
             />
-            <CustomDropDown
+            <ExpenseDropDown
               options={savingsTypes}
               placeholder="Transaction Type"
               onSelectValue={value => setTransactionTypeValue(value)}
             />
             <CustomTextInput
+              containerStyle={{minHeight: 60, borderRadius: 15}}
+              textArea={true}
               placeholder="Description"
-              placeholderTextColor="grey"
+              placeholderTextColor={COLORS.grey}
               onChangeText={value => setDescriptionValue(value)}
             />
-            {/* <AddAttachment
-                      title="Add Attachment"
-                      onPress={handleAttachment}
-                    />  */}
+
             <DateSelect
               placeholder="Select date"
               onSelectDate={value => setDateValue(value)}
             />
-            <RepeatTransaction title="Repeat" subTitle="Repeat Transaction" />
-          </View>
 
-          <CustomButton
-            title="Continue"
-            onPress={handleSubmit}
-            // Style={styles.button}
-            // titleStyle={styles.ButtonText}
-            Style={[styles.button]}
-            titleStyle={[styles.ButtonText]}
-            backgroundColor={COLORS.primary}
-          />
+            <CustomButton
+              title="Continue"
+              onPress={handleSubmit}
+              Style={[styles.button]}
+              titleStyle={[styles.ButtonText]}
+              backgroundColor={COLORS.primary}
+            />
+          </KeyboardAvoidingView>
         </View>
       </View>
     </View>
@@ -195,20 +449,27 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     backgroundColor: COLORS.green,
-    flexBasis: '100%',
+    paddingTop: SIZES.STATUSBAR_HEIGHT,
+  },
+  customSelect: {
+    width: '100%',
+    height: 55,
+    borderRadius: 15,
+    borderColor: 'gray',
+    borderWidth: 2,
   },
   topSection: {
-    flex: 3,
-    marginVertical: 22,
-    marginHorizontal: 20,
+    height: SIZES.height * 0.3,
+    marginHorizontal: 10,
+    justifyContent: 'space-between',
     // Replace with your desired styles
   },
   bottomSection: {
-    flex: 7,
-    backgroundColor: 'white',
+    backgroundColor: COLORS.white,
+    height: SIZES.height * 0.7 - SIZES.STATUSBAR_HEIGHT,
     borderTopLeftRadius: 40,
     borderTopRightRadius: 40,
-    //justifyContent:'space-between'
+    elevation: 1,
   },
   expense: {
     fontSize: 25,
@@ -218,33 +479,25 @@ const styles = StyleSheet.create({
   },
   input: {
     backgroundColor: 'transparent',
-    fontSize: 75,
+    fontSize: 65,
     fontWeight: 'bold',
     color: 'white',
   },
   textInput: {
     //marginTop: 1,
-    width: SIZES.width / 1.4,
+    width: SIZES.width / 1.1,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'flex-start',
   },
   button: {
     width: '100%',
-    // minHeight: 60,
-    // backgroundColor: COLORS.primary,
-    // justifyContent: 'center',
-    // alignItems: 'center',
-    // marginBottom: 5,
-    // borderRadius: 15,
+    marginTop: 20,
     elevation: 8,
-    // marginTop:6,
-    // zIndex:0
-    // width: SIZES.width - 30,
     minHeight: 60,
     justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: 5,
+
     borderRadius: 15,
   },
   ButtonText: {
